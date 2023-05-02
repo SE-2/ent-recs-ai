4389bcc4d91bf7445798ab21ad5d826dab1aa17c
Alireza Saei <saeialireza359@gmail.com>
2023-05-02 15:28:40 +0430

    Getting sample request and returning json

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
+import json
 
+# now we wait for backend team to define requests
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+@app.route('/api/user', methods=['GET'])
+def user():
+    name = request.args.get('name')
+    if name:
+        person = Person(name=name, age=30)
+        return json.dumps(person.__dict__)
+    else:
+        return 'Please provide a name.'
 
 if __name__ == '__main__':
     app.run(debug=True)
