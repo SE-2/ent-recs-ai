a4e8f0296437ca1a6970e39adcb5321e6b1195e4
Alireza Saei <saeialireza359@gmail.com>
2023-05-02 15:33:24 +0430

    Added simple KNN with users

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
-import json
+from flask import Flask, request, jsonify
+import pandas as pd
+from sklearn.neighbors import NearestNeighbors
 
 # now we wait for backend team to define requests
 app = Flask(__name__)
@@ -9,14 +10,24 @@ class Person:
         self.name = name
         self.age = age
 
-@app.route('/api/user', methods=['GET'])
-def user():
-    name = request.args.get('name')
-    if name:
-        person = Person(name=name, age=30)
-        return json.dumps(person.__dict__)
-    else:
-        return 'Please provide a name.'
+data = []
+X = data
+knn = NearestNeighbors(n_neighbors=5)
+knn.fit(X)
+
+@app.route('/api/similar_users', methods=['GET'])
+def get_similar_users():
+    user_id = int(request.args.get('user_id'))
+    if not user_id:
+        return jsonify({'error': 'Please provide a user ID.'}), 400
+    
+    user_index = data[data['id'] == user_id].index[0]
+    distances, indices = knn.kneighbors(X.iloc[user_index].values.reshape(1, -1))
+    
+    neighbor_user_ids = [data.iloc[index]['id'] for index in indices.squeeze()]
+
+    return jsonify({'Similar users': neighbor_user_ids})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
