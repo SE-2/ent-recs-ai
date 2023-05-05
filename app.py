110936bfdfcf36b8330a82e91423b391bf484251
Alireza Saei <saeialireza359@gmail.com>
2023-05-05 13:44:31 +0430

    Get JSON and read its data

--- app.py
+++ app.py
@@ -2,35 +2,34 @@ from flask import Flask, request, jsonify
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 
-# now we wait for backend team to define requests
 app = Flask(__name__)
 
-DATASET_LINKS = []
-books_data = pd.read_csv('books.csv')
-music_data = pd.read_csv('music.csv')
-movies_data = pd.read_csv('movies.csv')
-podcasts_data = pd.read_csv('podcasts.csv')
+# DATASET_LINKS = []
+# books_data = pd.read_csv('books.csv')
+# music_data = pd.read_csv('music.csv')
+# movies_data = pd.read_csv('movies.csv')
+# podcasts_data = pd.read_csv('podcasts.csv')
 
 
 categories = ['Book', 'Music', 'Movie', 'podcast']
-data = []
-X = data
-knn = NearestNeighbors(n_neighbors=5)
-knn.fit(X)
 
-@app.route('/api/similar_users', methods=['GET'])
-def get_similar_users():
-    user_id = int(request.args.get('user_id'))
-    if not user_id:
-        return jsonify({'error': 'Please provide a user ID.'}), 400
+@app.route('/api/similar_items', methods=['POST'])
+def get_similar_items():
+    json_data = request.get_json()
+
+    if not json_data or 'category' not in json_data:
+        return jsonify({'Error': 'Please provide a category.'}), 400
     
-    user_index = data[data['id'] == user_id].index[0]
-    distances, indices = knn.kneighbors(X.iloc[user_index].values.reshape(1, -1))
+    category = json_data['category']
+    user_scores = json_data['user_scores']
     
-    neighbor_user_ids = [data.iloc[index]['id'] for index in indices.squeeze()]
+    if category not in categories:
+        return jsonify({'Error': 'Invalid category.'}), 400
+    
 
-    return jsonify({'Similar users': neighbor_user_ids})
 
+    # TODO: build similar_items to return
+    return jsonify({'Recommendation': similar_items})
 
 if __name__ == '__main__':
     app.run(debug=True)
