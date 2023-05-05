8f66536994f9316d3e4eb1bf54dc154cc8f9587b
Alireza Saei <saeialireza359@gmail.com>
2023-05-05 13:49:23 +0430

    Categorized functions

--- app.py
+++ app.py
@@ -2,13 +2,30 @@ from flask import Flask, request, jsonify
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 
+
+def get_similar_books(user_scores):
+    pass
+
+
+def get_similar_musics(user_scores):
+    pass
+
+
+def get_similar_movies(user_scores):
+    pass
+
+
+def get_similar_podcasts(user_scores):
+    pass
+
+
 app = Flask(__name__)
 
-# DATASET_LINKS = []
-# books_data = pd.read_csv('books.csv')
-# music_data = pd.read_csv('music.csv')
-# movies_data = pd.read_csv('movies.csv')
-# podcasts_data = pd.read_csv('podcasts.csv')
+DATASET_LINKS = []
+books_data = pd.read_csv('books.csv')
+music_data = pd.read_csv('music.csv')
+movies_data = pd.read_csv('movies.csv')
+podcasts_data = pd.read_csv('podcasts.csv')
 
 
 categories = ['Book', 'Music', 'Movie', 'podcast']
@@ -22,13 +39,21 @@ def get_similar_items():
     
     category = json_data['category']
     user_scores = json_data['user_scores']
-    
+
     if category not in categories:
         return jsonify({'Error': 'Invalid category.'}), 400
-    
 
+    if json_data['category'] == 'Book':
+        similar_items = get_similar_books(user_scores)
+    elif json_data['category'] == 'Music':
+        similar_items = get_similar_musics(user_scores)
+    elif json_data['category'] == 'Movie':
+        similar_items = get_similar_movies(user_scores)
+    elif json_data['category'] == 'Podcast':
+        similar_items = get_similar_podcasts(user_scores)
+    else:
+        return jsonify({'Error': 'Invalid Category.'}), 400
 
-    # TODO: build similar_items to return
     return jsonify({'Recommendation': similar_items})
 
 if __name__ == '__main__':
