f1a74d27aff35f0b544b340b2650b4a430713d6e
Alireza Saei <saeialireza359@gmail.com>
2023-05-08 14:43:27 +0430

    Fixed a typo and extracted unique book genres

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 import numpy as np
+import ast
 
 def get_similar_books(user_scores):
     IDs = np.random.randint(0, 91, size=20)
@@ -26,11 +27,36 @@ app = Flask(__name__)
 
 DATASET_LINKS = []
 books_data = pd.read_csv('datasets\collaborative_book_metadata.csv')
+# Unique Book generes (10)
+# 'young-adult'
+# 'poetry'
+# 'fantasy, paranormal'
+# 'non-fiction'
+# 'mystery, thriller, crime'
+# 'children'
+# 'romance'
+# 'comics, graphic'
+# 'history, historical fiction, biography'
+# 'fiction'
+# ------------------------------------------------------
+# Code for extracting unique genres via set
+# setofgenres = []
+# for i in books_data.values:
+#     my_list = ast.literal_eval(i[8])
+#     print(my_list)
+#     for gnr in my_list:
+#         setofgenres.append(gnr)
+
+# print(len(setofgenres))
+# print(len(set(setofgenres)))
+# print(set(setofgenres))
+# ------------------------------------------------------
+
 music_data = pd.read_csv('datasets\Spotify_Youtube.csv')
 movies_data = pd.read_csv('datasets\imdb_top_1000.csv')
 podcasts_data = pd.read_json('datasets/podcast.json')
 
-categories = ['Book', 'Music', 'Movie', 'podcast']
+categories = ['Book', 'Music', 'Movie', 'Podcast']
 
 @app.route('/api/similar_items', methods=['POST'])
 def get_similar_items():
