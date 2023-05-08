7b53b052bdc06b601164bd2e52bf39115f8cddf5
Alireza Saei <saeialireza359@gmail.com>
2023-05-08 15:39:48 +0430

    SE2-231 Added invalid category test

--- test.py
+++ test.py
@@ -7,5 +7,12 @@ class TestGetSimilarItems(unittest.TestCase):
         self.headers = {'Content-Type': 'application/json'}
 
 
+    def test_invalid_category(self):
+        data = {'category': 'Invalid', 'user_scores': [0.5, 0.3, 0.8]}
+        response = requests.post(url=self.url, headers=self.headers, json=data)
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('Invalid category.', response.json()['Error'])
+
+
 if __name__ == '__main__':
     unittest.main()
