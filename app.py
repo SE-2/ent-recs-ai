cf4b01b4aadf8a5f8fe4927d84884fa9359772f2
Alireza Saei <saeialireza359@gmail.com>
2023-05-26 19:56:54 +0430

    Added kmeans book recommendation

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
+from sklearn.cluster import KMeans
 from flask import Flask, request, jsonify
 import pandas as pd
 import numpy as np
 import ast
 
+book_genres = ['young-adult', 'poetry', 'fantasy, paranormal', 'non-fiction', 
+               'mystery, thriller, crime', 'children', 'romance', 'comics, graphic', 
+               'history, historical fiction, biography', 'fiction']
+
+
+def get_book_vectors():
+    book_vectors = []
+    for i in books_data.values:
+        genres = ast.literal_eval(i[8])
+        vector = [10 if g in genres else 0 for g in book_genres]
+        book_vectors.append(vector)
+    return np.array(book_vectors)
+
+
 def get_similar_books(user_scores):
-    IDs = np.random.randint(0, 91, size=20)
-    return IDs
+    user_scores = np.array(user_scores)
+    books = get_book_vectors()
+
+    n_clusters = 10
+    kmeans = KMeans(n_clusters=n_clusters)
+    kmeans.fit(books)
+    
+    cluster_label = kmeans.predict(user_scores.reshape(1, -1))[0]
+    
+    book_cluster_labels = kmeans.labels_
+    book_ids_in_cluster = np.where(book_cluster_labels == cluster_label)[0][:5]
+
+    return book_ids_in_cluster.tolist()
 
 
 def get_similar_musics(user_scores):
@@ -92,4 +118,5 @@ def get_similar_items():
     return jsonify({'Recommendation': similar_items, 'Category': json_data['category']})
 
 if __name__ == '__main__':
+    print(get_similar_books([4.123,5.55,8.123,1,1,1,1.123,2.123, 7.7, 8.88]))
     app.run(debug=True)
