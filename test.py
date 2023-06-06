2aec110b3804ad6fd081504e73898d0ac72c2232
Alireza Saei <saeialireza359@gmail.com>
2023-06-06 09:42:01 +0430

    Changed testcases structure

--- test.py
+++ test.py
@@ -1,32 +1,13 @@
-import unittest
-import requests
+import pandas as pd
+import numpy as np
+from unittest import TestCase
+from Recommender import Recommender
 
-class TestGetSimilarItems(unittest.TestCase):
+class TestRecommender(TestCase):
+
     def setUp(self):
-        self.url = 'http://localhost:5000/api/similar_items'
-        self.headers = {'Content-Type': 'application/json'}
-
-
-    def test_invalid_category(self):
-        data = {'category': 'Invalid', 'user_scores': [0.5, 0.3, 0.8]}
-        response = requests.post(url=self.url, headers=self.headers, json=data)
-        self.assertEqual(response.status_code, 400)
-        self.assertIn('Invalid category.', response.json()['Error'])
-
-
-    def test_missing_category(self):
-        data = {'user_scores': [0.5, 0.3, 0.8]}
-        response = requests.post(url=self.url, headers=self.headers, json=data)
-        self.assertEqual(response.status_code, 400)
-        self.assertIn('Please provide a category.', response.json()['Error'])
-
-    # scores must be valid and in range 0 to 10
-    def test_invalid_user_scores(self):
-        data = {'category': 'Book', 'user_scores': [0.5, 20.0, 'invalid']}
-        response = requests.post(url=self.url, headers=self.headers, json=data)
-        self.assertEqual(response.status_code, 400)
-        self.assertIn('Invalid user scores', response.json()['Error'])
-
-
-if __name__ == '__main__':
-    unittest.main()
+        self.books_data = pd.read_csv('datasets/books_dataset.csv')
+        self.musics_data = pd.read_csv('datasets\musics_dataset.csv')
+        self.movies_data = pd.read_csv('datasets/movies_dataset.csv', encoding='ISO-8859-1')
+        self.podcasts_data = pd.read_json('datasets/podcast.json')
+        self.recommender = Recommender(self.podcasts_data, self.musics_data, self.movies_data, self.books_data)
