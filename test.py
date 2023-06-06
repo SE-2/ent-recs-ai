3d7f0d3b7489f5a76ed5a694b09ed2af264a0f9d
Mohammad Hossein Dehghani <64871739+MohammadHDehghani@users.noreply.github.com>
2023-06-06 20:32:29 +0330

    Update test.py

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ class TestRecommender(unittest.TestCase):
 
     def setUp(self):
         self.books_data = pd.read_csv('datasets/books_dataset.csv')
-        self.musics_data = pd.read_csv('datasets\musics_dataset.csv')
+        self.musics_data = pd.read_csv('datasets/musics_dataset.csv')
         self.movies_data = pd.read_csv('datasets/movies_dataset.csv', encoding='ISO-8859-1')
         self.podcasts_data = pd.read_json('datasets/podcast.json')
         self.recommender = Recommender(self.podcasts_data, self.musics_data, self.movies_data, self.books_data)
